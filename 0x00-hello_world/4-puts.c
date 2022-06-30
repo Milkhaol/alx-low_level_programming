ce013e32f699fb93ccd30ea1e180a5183d27f804
Milkhaol <kayodemubarak30@gmail.com>
2022-06-30 15:58:39 -0700

    tasks 2-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - this function prints out a message
+ * Return: the function returns a value of 0 when it runs successfully
+ */
+#include <stdio.h>
+int main(void)
+{
+		char message[] = "\"Programming is like building a multilingual puzzle";
+
+		puts(message);
+		return (0);
+}
