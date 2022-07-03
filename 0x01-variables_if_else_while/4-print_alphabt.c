a93fa07c97e0c0fb462fefb39400a1d77ae86a2f
Milkhaol <kayodemubarak30@gmail.com>
2022-07-03 20:42:47 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets except e and q
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	while (a <= 'd' || a >= 'f'  && a <= 'p' || a >= 'r')
+	{
+		putchar(a);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
