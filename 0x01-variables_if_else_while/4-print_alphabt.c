7592b6504974233516c1e7bc9c1d0726bb8de8b7
Milkhaol <kayodemubarak30@gmail.com>
2022-07-03 21:05:24 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,17 @@
  */
 int main(void)
 {
-	int a;
+	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
-		if (a <= 'd' || a >= 'f'  && a <= 'p' || a >= 'r')
-		putchar(a);
-	}
-	{
-		putchar('\n');
-	}
+		if (a != 'e'  && a != 'q')
+		{
+			putchar(a);
+		}
+		{
+			putchar('\n');
+		}
 		return (0);
+	}
 }
