86fe3daa22c0f234846ca8722321081584710d67
Milkhaol <kayodemubarak30@gmail.com>
2022-07-04 03:07:17 +0000

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out a statement followed by a new line
+ *
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
