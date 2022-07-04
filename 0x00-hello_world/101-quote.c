546b369e29b94459dac8e48b22bc7b8d2b8add4c
Milkhaol <kayodemubarak30@gmail.com>
2022-07-04 03:40:56 +0000

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - prints out a statement followed by a new line
@@ -8,9 +9,7 @@
  */
 int main(void)
 {
-	void write();
-
-	write("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
