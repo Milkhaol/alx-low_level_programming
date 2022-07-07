c6cecfcce8c702c4bd61fc10cc06f938d76da89f
Milkhaol <kayodemubarak30@gmail.com>
2022-07-07 21:21:10 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - cause an infinite loop
  * Return: 0
@@ -11,7 +12,9 @@ int main(void)
 	{
 		printf("Infinite loop incoming :(\n");
 	}
+	
 	i = 0;
+	
 	while (i < 10)
 	{
 		putchar(i);
