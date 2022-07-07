2396ef0484bb8ef4004a3f3a820c86036faf7016
Milkhaol <kayodemubarak30@gmail.com>
2022-07-07 21:36:37 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,7 @@ int main(void)
 {
 	/* int i; */
 
-	{
-		printf("Infinite loop incoming :(\n");
-	}
+	printf("Infinite loop incoming :(\n");
 
 	/**
 	 * i = 0;
@@ -19,8 +17,6 @@ int main(void)
 	 *	putchar(i);
 	 * }
 	 */
-	{
-		printf("Infinite loop avoided! \\o/\n");
-	}
-		return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
