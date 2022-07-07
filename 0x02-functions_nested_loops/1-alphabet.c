d18323b13cf63a32d0134a8b28e42560bc347054
Milkhaol <kayodemubarak30@gmail.com>
2022-07-07 15:10:27 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char *alpha = 97;
+
+	while (*alpha < 123)
+	{
+		_putchar(*alpha);
+		*alpha++;
+	}
+	_putchar('\n');
+}
