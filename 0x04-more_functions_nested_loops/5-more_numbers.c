de7ba5fc2acf5c6208a959352ba8e516e5c149ec
Milkhaol <kayodemubarak30@gmail.com>
2022-07-08 22:03:00 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 10 times
+ */
+
+void more_numbers(void)
+{
+	int b, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b =0; b < 15; b++)
+		{
+			if (b > 9)
+			_putchar(49);
+			_putchar((b % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
