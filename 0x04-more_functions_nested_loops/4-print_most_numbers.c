e7b54a273e4624d4752bad6abc63d52e10099465
Milkhaol <kayodemubarak30@gmail.com>
2022-07-08 22:43:25 +0000

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,14 @@
  *
  * Return: void
  */
+
 void print_most_numbers(void)
 {
-	for (c = 48; c <= 58; c++)
+	int c;
+
+	for (c = 48; c < 58; c++)
 	{
-		if (c != 2 && c != 4)
+		if (c != 50 && c != 52)
 		{
 		_putchar(c);
 		}
