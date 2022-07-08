773ecddd3cf4cd05e83be7e587f6094b20ec896b
Milkhaol <kayodemubarak30@gmail.com>
2022-07-08 22:40:56 +0000

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,12 +3,11 @@
 /**
  * print_most_numbers - prints digits from 0-9
  * except 2 and 4.
+ *
  * Return: void
  */
 void print_most_numbers(void)
 {
-	int c;
-
 	for (c = 48; c <= 58; c++)
 	{
 		if (c != 2 && c != 4)
