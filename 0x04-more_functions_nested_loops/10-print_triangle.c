476dae72ac657e60434660c72c434bc0c1186e3f
Milkhaol <kayodemubarak30@gmail.com>
2022-07-08 23:09:57 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangles
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			for (j = 0;  
+				_putchar(35);
+		}
+		_putchar('\n');
+
+	}
+}
